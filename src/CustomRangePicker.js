486755ebabdba50494f7374f77766f07491d32e6
penta-kihyun <71832733+kihyun1998@users.noreply.github.com>
2023-08-01 14:54:59 +0900

    disable time

--- src/CustomRangePicker.js
+++ src/CustomRangePicker.js
@@ -16,17 +16,17 @@ const CustomRangePicker = () => {
     if (start){
       if (date == null){
         setDate( [now, null] )
-      }else if (date[1] == null){
+      } else if (date[1] == null){
         setDate( [now, null] )
-      }else {
+      } else {
         setDate( [now,date[1]])
       }
     }else if (end){
       if (date == null){
         setDate( [null, now] )
-      }else if (date[0] == null){
+      } else if (date[0] == null){
         setDate( [null, now] )
-      }else {
+      } else {
         setDate( [date[0], now])
       }
     }
@@ -35,10 +35,10 @@ const CustomRangePicker = () => {
   const saveDate = (dates) => {
     if (dates == null){
       setDate(null)
-    }else{
+    } else{
       setDate([dates[0],dates[1]])
     }
-  }
+  };
 
   const footer = () => { 
     return (
@@ -48,22 +48,47 @@ const CustomRangePicker = () => {
     );
   };
 
-  const disabledRangeTime = (_,type) => {
-    if (type == 'start'){
+  const range = (start, end) => {
+    let array = [];
+    for (let i = start; i < end; ++i) {
+      array.push(i);
+    }
+    return array;
+  };
+
+  const disabledRangeDate = (current) => {
+    return current && current < dayjs("1969-12-31").endOf('day');
+  };
+
+  const disabledRangeTime = ( selDate , type) => {
+    // type 선택
+    if (type === 'start'){
       setStart(true)
       setEnd(false)
-    }else if(type == 'end'){
+    } else if(type === 'end'){
       setStart(false)
       setEnd(true)
     }
-  }
+
+    if (selDate <= dayjs("1970-01-01").endOf('day')){
+      return {
+        disabledHours: () => range(0,9)
+      }
+    }
+    
+
+    
+  };
 
   return (
     <Space direction="vertical" size={12}>
       <RangePicker
         renderExtraFooter={footer}
+        disabledDate={disabledRangeDate}
         disabledTime={disabledRangeTime}
-        showTime
+        showTime={{
+          hideDisabledOptions:true,
+        }}
         allowClear={true}
         value={date}
         // onChange={setDate}
