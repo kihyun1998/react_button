43f4dd559f621ff7a8536489df6713962c5123dd
penta-kihyun <71832733+kihyun1998@users.noreply.github.com>
2023-08-01 14:01:56 +0900

    현재시간 기능 변경

--- src/CustomRangePicker.js
+++ src/CustomRangePicker.js
@@ -7,21 +7,29 @@ const { RangePicker } = DatePicker;
 
 const CustomRangePicker = () => {
   const [date,setDate] = useState(null);
+  const [start,setStart] = useState(false);
+  const [end,setEnd] = useState(false);
 
   const onButtonClick = () => {
     const now = dayjs();
 
-    if (date==null){
-      setDate([now,null])
-    }else if (date[1]==null){
-      setDate([date[0],now])
-    }else if (date[0]==null){
-      setDate([now,date[1]])
-    }else{
-      setDate(now,null)
+    if (start){
+      if (date == null){
+        setDate( [now, null] )
+      }else if (date[1] == null){
+        setDate( [now, null] )
+      }else {
+        setDate( [now,date[1]])
+      }
+    }else if (end){
+      if (date == null){
+        setDate( [null, now] )
+      }else if (date[0] == null){
+        setDate( [null, now] )
+      }else {
+        setDate( [date[0], now])
+      }
     }
-    
-
   };
 
   const saveDate = (dates) => {
@@ -32,7 +40,6 @@ const CustomRangePicker = () => {
     }
   }
 
-
   const footer = () => { 
     return (
       <div onClick={onButtonClick} className='btn-time'>
@@ -41,10 +48,21 @@ const CustomRangePicker = () => {
     );
   };
 
+  const disabledRangeTime = (_,type) => {
+    if (type == 'start'){
+      setStart(true)
+      setEnd(false)
+    }else if(type == 'end'){
+      setStart(false)
+      setEnd(true)
+    }
+  }
+
   return (
     <Space direction="vertical" size={12}>
       <RangePicker
         renderExtraFooter={footer}
+        disabledTime={disabledRangeTime}
         showTime
         allowClear={true}
         value={date}
