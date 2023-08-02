5783ab7c785cea4d91d6ae9ad4aedb24ede7aadf
penta-kihyun <71832733+kihyun1998@users.noreply.github.com>
2023-08-02 13:20:29 +0900

    panel 초기화 기능 구현

--- src/CustomRangePicker.js
+++ src/CustomRangePicker.js
@@ -9,19 +9,33 @@ const CustomRangePicker = () => {
   const [date,setDate] = useState(null);
   const [start,setStart] = useState(false);
   const [end,setEnd] = useState(false);
+  const [mode, setMode] = useState([date,date]);
+  const [notDateMode, setNotDateMode] = useState(false);
+  const [first,setFirst] = useState(true);
+
+  const range = (start, end) => {
+    let array = [];
+    for (let i = start; i < end; ++i) {
+      array.push(i);
+    }
+    return array;
+  };
+
 
   const onButtonClick = () => {
     const now = dayjs();
 
-    if (start){
+    if (start){       // 왼쪽
       if (date == null){
         setDate( [now, null] )
       } else if (date[1] == null){
         setDate( [now, null] )
+      } else if (date[0] == null){
+        setDate( [now,date[1]])  
       } else {
         setDate( [now,date[1]])
       }
-    }else if (end){
+    }else if (end){   // 오른쪽
       if (date == null){
         setDate( [null, now] )
       } else if (date[0] == null){
@@ -30,6 +44,7 @@ const CustomRangePicker = () => {
         setDate( [date[0], now])
       }
     }
+    setMode(['date','date'])
   };
 
   const saveDate = (dates) => {
@@ -48,15 +63,8 @@ const CustomRangePicker = () => {
     );
   };
 
-  const range = (start, end) => {
-    let array = [];
-    for (let i = start; i < end; ++i) {
-      array.push(i);
-    }
-    return array;
-  };
-
   const disabledRangeDate = (current) => {
+    // 날짜 금지
     return current && current < dayjs("1969-12-31").endOf('day');
   };
 
@@ -70,30 +78,55 @@ const CustomRangePicker = () => {
       setEnd(true)
     }
 
+    // 시간 금지
     if (selDate <= dayjs("1970-01-01").endOf('day')){
       return {
         disabledHours: () => range(0,9)
       }
     }
-    
-
-    
   };
+
+  const modeChange = (_, newMode) => {
+    if (newMode == null){
+      return;
+    }else{
+      setMode(newMode)
+      console.log(newMode)
+      const chkIsDate = (el) => el !== 'date';
+      if (newMode.some(chkIsDate)){
+        setNotDateMode(true)
+      }
+    }
+  }
+
+  const returnMode = () => {
+    if (notDateMode){
+      setMode(['date','date'])
+    }else if(first){
+      setFirst(false)
+      setMode(['date','date'])
+    }
+  }
 
   return (
     <Space direction="vertical" size={12}>
       <RangePicker
+        allowClear={true}
+        size='small'
         renderExtraFooter={footer}
+
         disabledDate={disabledRangeDate}
         disabledTime={disabledRangeTime}
         showTime={{
           hideDisabledOptions:true,
         }}
-        allowClear={true}
+
         value={date}
-        // onChange={setDate}
         onCalendarChange={saveDate}
-        size='small'
+
+        mode={mode}
+        onPanelChange={modeChange}
+        onFocus={returnMode}
       />
     </Space>
   );
