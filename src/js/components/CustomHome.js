0fa3b7e13418c21d2f2157c3b2c660c2c2b7ee6c
penta-kihyun <71832733+kihyun1998@users.noreply.github.com>
2023-08-24 15:01:40 +0900

    커스텀 홈 생성

---
+++ src/js/components/CustomHome.js
@@ -0,0 +1,17 @@
+/* eslint-disable*/
+import '../../css/App.css';
+import React from "react";
+
+
+function CustomHome() {
+    return (
+        <div className='table'>
+            <h1>
+                Hello, This is Main page!
+            </h1>
+
+        </div>
+    );
+}
+
+export default CustomHome;
